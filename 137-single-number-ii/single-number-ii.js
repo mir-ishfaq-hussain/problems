e39c98067917e9197ee27c4f3a575d7028a7a8c5
Mir Ishfaq Hussain <106236454+mir-ishfaq-hussain@users.noreply.github.com>
2024-07-03 09:32:30 +0530

    Time: 65 ms (27.43%) | Memory: 51.6 MB (29.75%) - LeetSync

---
+++ 137-single-number-ii/single-number-ii.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+let hash ={}
+for(let num of nums){
+if(hash[num]){
+    hash[num] = hash[num]  +1
+}
+else{
+    hash[num] = 1
+}
+}
+
+for(num of nums){
+    if(hash[num]===1){
+        return num
+    }
+}
+};
